352488277ea6c47ca0c554c626d8c648129c88b9
Sebastian Triana martinez <sebastian.triana@utp.edu.co>
2024-04-15 01:23:27 -0500

    cors

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const app = express();
 const ports = [8021, 4499];
+const cors = require('cors');
 const routerApi = require('./routes');
 
 const {errorHandler, boomErrorHandler} = require('./core/middlewares/error.handler');
 
 app.use(express.json());
+app.use(cors({
+    origin: 'http://localhost:8096, http://localhost:8021, http://localhost:8021',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type,Authorization',
+  }));
 
 app.get('/', (req, res) => {
     res.json({
